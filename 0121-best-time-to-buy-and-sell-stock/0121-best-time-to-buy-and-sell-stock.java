25a3a6446cba2f1b91f2eeed660141cd51957fa6
Anjani singh <antoniocristiano454@gmail.com>
2026-08-13 11:53:29 +0530

    Time: 1 ms (99.96%), Space: 94.4 MB (51.83%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,27 +1,13 @@
 class Solution {
-    static int n;
-     static Integer[][] dp;
     public int maxProfit(int[] prices) {
-        n = prices.length;
-        dp = new Integer[n][3];
-        return func(0, 0, prices);
-    }
-    static int func(int i, int t, int[] nums) {
-        if (i >= n || t == 2)  return 0;
+        int maxProfit=0, bestbuy = prices[0];
 
-        if (dp[i][t] != null)  return dp[i][t];
-
-        int a=0, b=0;
-
-        if (t == 0) {   
-           a = -nums[i] + func(i + 1, 1, nums);
-           b = func(i + 1, t, nums);
-        } 
-        if(t==1){       
-           a = nums[i] + func(i + 1, 2, nums);
-           b = func(i + 1, t, nums);
+        for(int i = 0; i< prices.length ; i++){
+            if(prices[i] > bestbuy){
+                maxProfit = Math.max(maxProfit,prices[i]-bestbuy);
+            }
+            bestbuy = Math.min(bestbuy,prices[i]);
         }
-        return dp[i][t]= Math.max(a, b);
+        return maxProfit;
     }
 }
-
